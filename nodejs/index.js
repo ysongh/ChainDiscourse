8bb9934ca86cbe8ef7200f05aaaf5e08e8ac63ab
You Song Hou <ysongweb3@gmail.com>
2024-05-24 00:35:15 -0400

    feat: install @lit-protocol/lit-node-client

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const LitJsSdk = require("@lit-protocol/lit-node-client");
 
 const app = express();
 
@@ -8,7 +9,18 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(cors());
 
+app.locals.litNodeClient = new LitJsSdk.LitNodeClientNodeJs({
+  alertWhenUnauthorized: false,
+  litNetwork: 'habanero',
+});
+
+
 app.get('/', (req, res) => res.send('It Work'));
+
+app.get('/connect', async (req, res) => {
+  await app.locals.litNodeClient.connect();
+  res.send('Connecting');
+});
 
 const port = process.env.PORT || 4000;
 
