e0efa428c2dd3ab27c2e8a0e20891c75790de9df
You Song Hou <ysongweb3@gmail.com>
2024-05-27 01:15:33 -0400

    refactor: remove App.css

--- reactjs/src/App.jsx
+++ reactjs/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { HashRouter, Route, Routes } from 'react-router-dom';
 
-import './App.css';
 import Home from './page/Home';
 import Chats from './page/Chats';
 
