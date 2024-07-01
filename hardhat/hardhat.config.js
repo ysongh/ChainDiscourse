798f00e62da8cd316898f647e646e822e9d2df7c
You Song Hou <ysongweb3@gmail.com>
2024-07-01 00:29:00 -0400

    feat: update hardhat.config.js to deploy a contract on Meter Testnet

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -4,6 +4,12 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    "metertestnet": {
+      url: "https://rpctest.meter.io",
+      accounts: [process.env.PRIVATEKEY],
+    },
+  },
   paths: {
     artifacts: '../reactjs/src/artifacts',
     cache: '../reactjs/src/cache',
